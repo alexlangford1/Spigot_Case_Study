2d52f712eb04ba0f7687280e3d6c5f5edccd1d91
alex langford <alejandro010795@gmail.com>
2019-09-09 01:17:44 -0600

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            WebRequest webRequest = WebRequest.Create("https://imp.onesearch.org/impression.do?event=CaseStudy&source=AlexCaseStudy&traffic_source=AlexCaseStudy");
+            webRequest.Method = "Head";
+            return View();
+        }
+    }
+}
